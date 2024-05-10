7a602a53d45b590b1df4c26fea71b16d7285b0cd
nrusnac <nrusnac@tudelft.nl>
2024-05-10 19:12:47 +0200

    Created initial files

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='lib-ml',
+    version='0.1.0',  # Use a versioning scheme
+    packages=find_packages(),
+    install_requires=[
+        #TODO: add required packages here
+    ],
+)
