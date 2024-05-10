c00074feb05acbc71cbf3d98f081aed025ec5933
nrusnac <nrusnac@tudelft.nl>
2024-05-10 20:23:20 +0200

    Fix file structure again

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='lib-ml',
-    version='0.1.0',  # Use a versioning scheme
+    version='0.1.2',  # Use a versioning scheme
+    long_description=open('README.md').read(),
     packages=find_packages(),
+    url='https://github.com/remla24-team12/lib_ml',
     install_requires=[
         #TODO: add required packages here
     ],
